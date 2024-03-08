a0124375a158ea54be104d2a0683a25333c5ebb9
leoaugustosv <kleeniietv@gmail.com>
2024-03-08 19:54:55 -0300

    Commit inicial

---
+++ src/ContaTerminal.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner sc = new Scanner(System.in);
+        
+
+
+        System.out.println("Insira o número da conta abaixo: ");
+        int numeroConta = sc.nextInt();
+
+        System.out.println("Insira a agência: ");
+        String codigoAgencia = sc.next();
+
+        System.out.println("Insira o nome do cliente: ");
+        String nomeCliente = sc.nextLine();
+        nomeCliente+=sc.nextLine();
+
+        
+
+
+        System.out.println("Insira o saldo inicial a ser depositado: ");
+        double saldoConta = sc.nextDouble();
+
+        System.out.println("\n\n\n");
+        System.out.printf("Olá, %S, obrigado por criar uma conta em nosso banco. Sua agência é %s, conta %s. Seu saldo atual é de R$ %.2f.",
+        nomeCliente, codigoAgencia, numeroConta, saldoConta);
+        
+        sc.close();
+        
+    }
+}
